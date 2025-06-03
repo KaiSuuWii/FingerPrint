43cdc94fd2bcff404de96a3bbed52728f0a596e4
KaiSuuWii <kaiv3n1.jeez@gmail.com>
2025-06-03 12:34:16 +0800

    update: created routes for the frontend

--- index.php
+++ index.php
@@ -7,4 +7,29 @@
 
 namespace fingerprint;
 
-require("./src/html/home.html");
+// Get the request URI
+$request_uri = $_SERVER['REQUEST_URI'];
+
+// Remove any query strings
+$request_uri = strtok($request_uri, '?');
+
+// Remove trailing slashes
+$request_uri = rtrim($request_uri, '/');
+
+// If empty, set to default
+if (empty($request_uri)) {
+    $request_uri = '/';
+}
+
+// Route handling
+switch ($request_uri) {
+    case '/register':
+        require("./src/html/register.html");
+        break;
+    case '/log':
+        require("./src/html/log.html");
+        break;
+    default:
+        require("./src/html/home.html");
+        break;
+}
